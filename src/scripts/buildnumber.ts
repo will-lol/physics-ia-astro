23294b680a07c5820a8ca9d33e7b832fe45d456c
William Bradshaw <will.bradshaw50@gmail.com>
2023-02-07 21:42:05 +1100

    fix build number?

--- src/scripts/buildnumber.ts
+++ src/scripts/buildnumber.ts
@@ -3,10 +3,15 @@ import { readFile, writeFile } from "fs/promises";
 export default async function getBuildNumberAndIncrement(): Promise<string | undefined> {
     try {
         const filePath = new URL('../../public/buildnumber', import.meta.url);
-        const contents = await readFile(filePath, { encoding: "utf8" });
+        let buildNumber: any;
+        try {
+          buildNumber = await readFile(filePath, { encoding: "utf8" });
+        } catch {
+          buildNumber = await fetch("../../public/buildnumber").then((res) => {res.json()});
+        }
 
-        writeFile(filePath, (parseInt(contents) + 1).toString())
-        return(contents);
+        writeFile(filePath, (parseInt(buildNumber) + 1).toString())
+        return(buildNumber);
       } catch (err) {
         console.error(err);
       }
